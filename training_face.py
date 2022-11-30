7acd7ed892ca61e22c2395c01a5626e417d0a7de
EdgarVivas <90181227+EdgarVivas@users.noreply.github.com>
2022-11-30 12:59:52 +0100

    Update training_face.py

--- training_face.py
+++ training_face.py
@@ -9,12 +9,14 @@ Names=[]
 
 image_dir='/home/aaunano/PyPro/faceRecognizer/demoImages/known'
 for root, dirs, files in os.walk(image_dir):
-    print(files)
+
     for file in files:
+        
         path=os.path.join(root,file)
-        print(path)
-        name=os.path.splitext(file)[0]
-        print(name)
+        #print(path)
+        
+        name=os.path.basename(os.path.dirname(path))
+        print(file)
         person=face_recognition.load_image_file(path)
         encoding=face_recognition.face_encodings(person)[0]
         Encodings.append(encoding)

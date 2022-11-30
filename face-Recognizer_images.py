6d818cbeed476dc322e0e41af5d404a8f589c494
MARO-445 <34687013+MARO-445@users.noreply.github.com>
2022-11-30 14:15:23 +0100

    Update face-Recognizer_images.py

--- face-Recognizer_images.py
+++ face-Recognizer_images.py
@@ -27,10 +27,9 @@ for root,dirs, files in os.walk(image_dir):
 		testImage=face_recognition.load_image_file(testImagePath)
 		facePositions=face_recognition.face_locations(testImage)
 		allEncodings=face_recognition.face_encodings(testImage,facePositions)
-		testImage=cv
-image_dir='/home/aaunano/PyPro/faceRecognizer/demoImages/unknown'
-for root,dirs, files in os.walk(image_dir):
-	for file in fr (top,right,bottom,left),face_encoding in zip(facePositions, allEncodings):
+		testImage=cv2.cvtColor(testImage,cv2.COLOR_RGB2BGR)
+
+		for (top,right,bottom,left),face_encoding in zip(facePositions, allEncodings):
 			name='Unknown person'
 			matches=face_recognition.compare_faces(Encodings,face_encoding)
 			if True in matches:
@@ -39,11 +38,10 @@ for root,dirs, files in os.walk(image_dir):
 			centery=(top+bottom)/2
 			centerx=(right+left)/2
 			cv2.rectangle(testImage,(left,top),(right,bottom),(0,0,255),2)
-
+			#cv2.putText(testImage,name,(left,top-6),font,.75,(0,255,255),2)
 
 			print(name , " is at x:",centerx , ", y:" , centery)
-		
-
-
-
-
+		#cv2.imshow('Picture', testImage)
+		#cv2.moveWindow('Picture',0,0)
+		#if cv2.waitKey(0)==ord('q'):
+		#	cv2.destroyAllWindows()

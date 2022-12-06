978b8125da3cc90417cda2fe36bf8e52766cacb5
Karlis Tregers <93842312+Ka-rlis@users.noreply.github.com>
2022-12-06 13:36:50 +0100

    Update motorModule.py

--- motorModule.py
+++ motorModule.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import SSDMobileNetModule as sm
-direction = 0
+directionX = 0
+directionY = 0
 GPIO.cleanup()
 # Direction pin from controller
 DIRH = 7   
-DIRV = 16
+DIRV = 11
 # Step pin from controller
 STEPH = 33
 STEPV = 32
@@ -25,19 +26,39 @@ GPIO.output(DIRH, CW)
 GPIO.output(DIRV, CW)
 class motorModules():
     #Move the motor during a number of steps
-    def move_MotorX(steps,speed):
+    def move_MotorX(stepsX,speed):
         # Set the first direction you want it to spin
-        global direction
-        if steps < 0:
-            direction = CW
-            steps = abs(steps)
-        elif steps > 0:
-            direction = CCW
+        global directionX
+        if stepsX < 0:
+            directionX = CW
+            stepsX = abs(stepsX)
+        elif stepsX > 0:
+            directionX = CCW
         else:
-            steps = 0
-        GPIO.output(DIRH, direction)
-        for x in range(steps // 20):
+            stepsX = 0
+        GPIO.output(DIRH, directionX)
+        for x in range(stepsX // 20):
                 GPIO.output(STEPH, GPIO.HIGH)
                 sleep(speed)
                 GPIO.output(STEPH, GPIO.LOW)
+                sleep(speed)
+
+
+
+    def move_MotorY(stepsY,speed):
+        # Set the first direction you want it to spin
+        global directionY
+        if stepsY > 0:
+            directionY = CW
+        elif stepsY < 0:
+            directionY = CCW
+            stepsY = abs(stepsY)
+        else:
+            stepsY = 0
+        print(stepsY)
+        GPIO.output(DIRV, directionY)
+        for x in range(stepsY // 20):
+                GPIO.output(STEPV, GPIO.HIGH)
+                sleep(speed)
+                GPIO.output(STEPV, GPIO.LOW)
                 sleep(speed)

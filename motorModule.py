ed79a9ef4dac2f7d8afd7340ddf94c11a314e549
Karlis Tregers <93842312+Ka-rlis@users.noreply.github.com>
2022-12-06 12:40:40 +0100

    Update motorModule.py

--- motorModule.py
+++ motorModule.py
@@ -1,22 +1,43 @@
-import SSDMobileNetModule as mnSSDm
-import cv2
-import motorModule as mM
 import RPi.GPIO as GPIO
-
-def main():
-    cap = cv2.VideoCapture(0)
-    cap.set(3,640)
-    cap.set(4,480)
-    myModel = mnSSDm.mnSSD("ssd-mobilenet-v2", 0.5)
-    try:
-        while True:
-            success, img = cap.read()
-            positionMotor = myModel.detect(img,True)[1]
-            cv2.imshow("Image", img)
-            cv2.waitKey(1)
-            mM.motorModules.move_MotorX(positionMotor, 0.0005)
-    finally: 
-        print("cleanup")
-        GPIO.cleanup()
-if __name__ == "__main__":
-    main()
+from time import sleep
+import SSDMobileNetModule as sm
+direction = 0
+GPIO.cleanup()
+# Direction pin from controller
+DIRH = 7   
+DIRV = 16
+# Step pin from controller
+STEPH = 33
+STEPV = 32
+# 0/1 used to signify clockwise or counterclockwise.
+CW = 1
+CCW = 0
+GPIO.setwarnings(False)
+# Setup pin layout on PI
+GPIO.setmode(GPIO.BOARD)
+# Establish Pins in software
+GPIO.setup(DIRH, GPIO.OUT)
+GPIO.setup(STEPH, GPIO.OUT)
+GPIO.setup(DIRV, GPIO.OUT)
+GPIO.setup(STEPV, GPIO.OUT)
+# Set the first direction you want it to spin
+GPIO.output(DIRH, CW)
+GPIO.output(DIRV, CW)
+class motorModules():
+    #Move the motor during a number of steps
+    def move_MotorX(steps,speed):
+        # Set the first direction you want it to spin
+        global direction
+        if steps < 0:
+            direction = CW
+            steps = abs(steps)
+        elif steps > 0:
+            direction = CCW
+        else:
+            steps = 0
+        GPIO.output(DIRH, direction)
+        for x in range(steps // 20):
+                GPIO.output(STEPH, GPIO.HIGH)
+                sleep(speed)
+                GPIO.output(STEPH, GPIO.LOW)
+                sleep(speed)

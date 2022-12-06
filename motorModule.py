e63d84142ea555a602872bc3be4b151e0493dbb9
Karlis Tregers <93842312+Ka-rlis@users.noreply.github.com>
2022-12-06 12:39:53 +0100

    Update motorModule.py

--- motorModule.py
+++ motorModule.py
@@ -1,54 +1,22 @@
+import SSDMobileNetModule as mnSSDm
+import cv2
+import motorModule as mM
 import RPi.GPIO as GPIO
-from time import sleep
-import SSDMobileNetModule as sm
 
-GPIO.cleanup()
-# Direction pin from controller
-DIRH = 7   
-DIRV = 16
-# Step pin from controller
-STEPH = 33
-STEPV = 32
-# 0/1 used to signify clockwise or counterclockwise.
-CW = 1
-CCW = 0
-
-# Setup pin layout on PI
-GPIO.setmode(GPIO.BOARD)
-# Establish Pins in software
-GPIO.setup(DIRH, GPIO.OUT)
-GPIO.setup(STEPH, GPIO.OUT)
-GPIO.setup(DIRV, GPIO.OUT)
-GPIO.setup(STEPV, GPIO.OUT)
-# Set the first direction you want it to spin
-GPIO.output(DIRH, CW)
-GPIO.output(DIRV, CW)
-class motorModules():
-    def move(x):
-    #    try:
-        if x < 0:
-            GPIO.output(DIRH, CW)
-            sleep(0.005)
-        elif x > 0:
-            GPIO.output(DIRH, CCW)
-            sleep(0.005)
-        else: pass
-        for x in range(50):
-            GPIO.output(STEPH, GPIO.HIGH)
-            sleep(0.0005)
-            GPIO.output(STEPH, GPIO.LOW)
-            sleep(0.0005)
-    #    except KeyboardInterrupt:
-    #        print("cleanup")
-    #        GPIO.cleanup()
-    # def movev(y):
-    #     if y > 0:
-    #         GPIO.output(DIRV, 1)
-    #     elif y < 0:
-    #         GPIO.output(DIRV, 0)
-    #     else: pass
-    #     sleep(0.1)
-    #     GPIO.output(STEPV, GPIO.HIGH)
-    #     sleep(.00005)
-    #     GPIO.output(STEPV, GPIO.LOW)
-    #     sleep(.00005)
+def main():
+    cap = cv2.VideoCapture(0)
+    cap.set(3,640)
+    cap.set(4,480)
+    myModel = mnSSDm.mnSSD("ssd-mobilenet-v2", 0.5)
+    try:
+        while True:
+            success, img = cap.read()
+            positionMotor = myModel.detect(img,True)[1]
+            cv2.imshow("Image", img)
+            cv2.waitKey(1)
+            mM.motorModules.move_MotorX(positionMotor, 0.0005)
+    finally: 
+        print("cleanup")
+        GPIO.cleanup()
+if __name__ == "__main__":
+    main()
